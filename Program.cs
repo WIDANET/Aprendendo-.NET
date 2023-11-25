8483f1939c8d7213a770372231ba33b56d8b8809
WIDANET <willianlps99@outlook.com>
2023-11-25 18:07:51 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,24 +1,34 @@
-﻿// *** OPERADORES CONDICIONAIS COM IF/ELSE *** 
+﻿// *** SWITCH CASE ***
 
-int quantidadeEmEstoque = 10;
 
-int quantidadeCompra = 48;
 
-// bool = variável booleana
-bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
 
-Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
-Console.WriteLine($"Quantidade comppra: {quantidadeCompra}");
-Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
 
-if(possivelVenda){
 
-    Console.WriteLine("Venda realizada!");
-}
-else{
+// *** OPERADORES CONDICIONAIS COM IF/ELSE *** 
 
-    Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque!");
-}
+// int quantidadeEmEstoque = 10;
+
+// int quantidadeCompra = 0;
+
+// // bool = variável booleana
+// bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+
+// Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidade comppra: {quantidadeCompra}");
+// Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+// if(quantidadeCompra == 0){
+//     Console.WriteLine("Venda inválida!");
+// }
+// else if(possivelVenda){
+
+//     Console.WriteLine("Venda realizada!");
+// }
+// else{
+
+//     Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque!");
+// }
 
 
 
