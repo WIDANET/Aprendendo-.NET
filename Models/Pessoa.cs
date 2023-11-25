9d0f9c9516b74560e723b2ac235f5872342923c0
WIDANET <willianlps99@outlook.com>
2023-11-25 16:24:04 -0300

    update

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -10,6 +10,11 @@ namespace Aprendendo_.NET.Models
         public string Nome { get; set; }
         public int Idade { get; set; }
 
+        public Pessoa(string Nome, int Idade){
+            this.Nome = Nome;
+            this.Idade = Idade;
+        }
+
         public void Apresentar (){
 
             Console.WriteLine($"Olá, meu nome é {Nome}, e eu tenho {Idade} anos.");
---
+++ aula1.cs
@@ -0,0 +1,11 @@
+
+
+// Fazendo um Cast para converter tipo de varáveis
+
+int a = Convert.ToInt32("26");
+
+Console.WriteLine(a);
+
+int b = int.Parse("24");
+
+Console.WriteLine(b);
