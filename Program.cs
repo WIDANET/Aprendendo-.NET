f08fd7f6a6c24720edbf0e239a1876a83fafc2fa
WIDANET <willianlps99@outlook.com>
2023-11-24 21:27:44 -0300

    testanto varáveis

--- Program.cs
+++ Program.cs
@@ -1,7 +1,29 @@
-﻿using Aprendendo_.NET.Models;
+﻿
 
-Pessoa p = new Pessoa();
+string texto = "Olá, tudo bem?";
 
-p.Nome = "Willian";
-p.Idade = 24;
-p.Apresentar();
+int quantidade = 1;
+
+double altura = 1.82;
+
+decimal preco = 1.80M;
+
+bool condicao = true;
+
+Console.WriteLine(texto);
+Console.WriteLine("O valor da variável condição é: " +quantidade);
+Console.WriteLine("O valor da variável altura é: " + altura);
+Console.WriteLine("O valor da variável preço é: " + preco);
+Console.WriteLine("O valor da variável condição é: " + condicao);
+
+
+
+
+
+// using Aprendendo_.NET.Models;
+
+// Pessoa p = new Pessoa();
+
+// p.Nome = "Willian";
+// p.Idade = 24;
+// p.Apresentar();
