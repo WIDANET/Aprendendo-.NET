bd98a2a713a46d8766d5849d3c279a99551d964b
WIDANET <willianlps99@outlook.com>
2023-11-26 21:45:31 -0300

    update

--- Models/Calculadora.cs
+++ Models/Calculadora.cs
@@ -53,5 +53,11 @@ namespace Aprendendo_.NET.Models
             double tangente = Math.Tan(radiano);
             Console.WriteLine($"Tangente de {angulo}º é {Math.Round(tangente, 4)}");
         }
+
+        public void RaizQuadrada(double x)
+        {
+           double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz quadrada de {x} é {raiz}");
+        }
     }
 }
