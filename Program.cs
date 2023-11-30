166c88234f39154fcd9da6b70eebcde5ab816118
WIDANET <willianlps99@outlook.com>
2023-11-29 21:01:52 -0300

    array

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 ﻿using Aprendendo_.NET.Models;
  //...
 
+ // *** INICIANDO ARRAYS ***
+
+int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+
+for(int i = 0; i < arrayInteiros.Length; i++)
+{
+    Console.WriteLine($"Posição Nº {i} - {arrayInteiros[i]}");
+}
+
 
 
 
