f2286dc53fb251ded4394db5f3d2adb5581872a2
WIDANET <willianlps99@outlook.com>
2023-11-29 21:13:56 -0300

    forEach

--- Program.cs
+++ Program.cs
@@ -9,9 +9,19 @@ arrayInteiros[0] = 72;
 arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
 
+Console.WriteLine("PERCORRENDO O ARRAY COM FOR");
+
 for(int i = 0; i < arrayInteiros.Length; i++)
 {
     Console.WriteLine($"Posição Nº {i} - {arrayInteiros[i]}");
+}
+
+Console.WriteLine("PERCORRENDO O ARRAY COM FOREACH");
+int contadorForEach = 0;
+foreach(int valor in arrayInteiros)
+{
+    Console.WriteLine($"Posição Nº {contadorForEach} - {valor}");
+    contadorForEach++;
 }
 
 
